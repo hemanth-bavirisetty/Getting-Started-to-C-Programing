b518bd8e0cc4cd8575729cc930a12fe3e2c18860
Bavirisetty Hemanth Manikanta <72919457+manikantahemanth@users.noreply.github.com>
2021-06-28 21:19:13 +0530

    Update display_datatypes.c

--- Data Types/display_datatypes.c
+++ Data Types/display_datatypes.c
@@ -8,6 +8,18 @@ Note-2: The format specifier for double is %lf and long double is %Lf.
 Note-3: Space before %c removes any white space (blanks, tabs, or newlines).
         It means %c without space will read white space like new line(\n), spaces(‘ ‘) or tabs(\t). 
         By adding space before %c, we are skipping this and reading only the char given.
+
+Expected Output:
+Enter·int,·short·int·and·long·int·values·:·32456 456 56789123
+Enter·float,·double·and·long·double·values·:·33.46572 234.56789 24156.78923
+Enter·a·character·:·S
+int·:·32456
+short·int·:·456
+long·int·:·56789123
+float·:·33.465721
+double·:·234.567890
+long·double·:·24156.789230
+character·:·S
 */
 #include<stdio.h>
 void main()
